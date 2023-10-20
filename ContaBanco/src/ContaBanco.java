261a53a9145e5d685d071a3e916562ddcd44a786
guilherme <guilherme.souza.1997@outlook.com>
2023-10-20 12:38:42 -0300

    projeto conta banco concluído

---
+++ ContaBanco/src/ContaBanco.java
@@ -0,0 +1,31 @@
+public class ContaBanco {
+    
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaBanco(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    // Getters
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner input = new Scanner(System.in);
+
+       System.out.println("Por favor digite o número da agência");
+       String agencia = input.nextLine();
+
+       System.out.println("Por favor digite o número da conta");
+       int numero = input.nextInt();
+       input.nextLine();
+
+       System.out.println("Por favor digite o nome do cliente");
+       String nomeCliente = input.nextLine();
+
+       System.out.println("Por favor digite o saldo");
+       double saldo = input.nextDouble();
+
+        ContaBanco conta = new ContaBanco(numero, agencia, nomeCliente, saldo);
+
+       System.out.println("\nInformações da Conta:");
+       System.out.println("Agência: " + conta.getAgencia());
+       System.out.println("Número da Conta: " + conta.getNumero());
+       System.out.println("Nome do Cliente: " + conta.getNomeCliente());
+       System.out.println("Saldo: " + conta.getSaldo());
+
+       input.close();
     }
 }
